87fb5156205420a7c07ca7bc9181a36ff1228d7f
miracle-halation <miraclehalation.9710@gmail.com>
2021-08-27 16:47:27 +0900

    ログインページのラフ準備

---
+++ pages/login/index.vue
@@ -0,0 +1,29 @@
+<template>
+	<v-app>
+		<v-card>
+			<v-card-title>
+				<h1 class="display-1">ログイン</h1>
+			</v-card-title>
+			<v-card-text>
+				<v-form>
+					<v-text-field label="ユーザ名" />
+					<v-text-field label="パスワード" />
+				</v-form>
+			</v-card-text>
+			<v-card-actions>
+				<v-btn>ログイン</v-btn>
+			</v-card-actions>
+		</v-card>
+	</v-app>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+
+})
+</script>
+
+<style scoped>
+
+</style>
