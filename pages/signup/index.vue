fd0a43cd06a0d2610d313b3a50c5e1cad68e7ae1
miracle-halation <miraclehalation.9710@gmail.com>
2021-08-27 16:53:25 +0900

    新規登録ページラフ準備

---
+++ pages/signup/index.vue
@@ -0,0 +1,32 @@
+<template>
+	<v-app>
+		<v-card>
+			<v-card-title>
+				<h1 class="display-1">新規登録</h1>
+			</v-card-title>
+			<v-card-text>
+				<v-form>
+					<v-text-field label="ユーザ名" />
+					<v-text-field label="メールアドレス" />
+					<v-text-field label="パスワード" />
+					<v-text-field label="パスワード確認" />
+					<v-file-input label="アイコン" />
+				</v-form>
+			</v-card-text>
+			<v-card-actions>
+				<v-btn>新規登録</v-btn>
+			</v-card-actions>
+		</v-card>
+	</v-app>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+
+})
+</script>
+
+<style scoped>
+
+</style>
