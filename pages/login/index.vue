ef4c036248363c3603739d2a29d652ebd02b91d1
miracle-halation <miraclehalation.9710@gmail.com>
2021-09-12 10:19:17 +0900

    ログインページにバリデーションを実装

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -5,18 +5,44 @@
 				<h1 class="display-1">ログイン</h1>
 			</v-card-title>
 			<v-card-text>
-				<v-form>
-					<v-text-field v-model="email" label="メールアドレス" />
-					<v-text-field v-model="password" label="パスワード"
-						:append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
-            :type="show1 ? 'text' : 'password'"
-            @click:append="show1 = !show1"
-					 />
-				</v-form>
+				<ValidationObserver
+					ref="observer"
+					v-slot="{ invalid }"
+				>
+					<v-form @submit.prevent="submit">
+						<ValidationProvider
+							v-slot="{ errors }"
+							name="メールアドレス"
+							rules="required|email"
+						>
+							<v-text-field
+								v-model="email"
+								label="メールアドレス"
+								:error-messages="errors"
+								required
+							/>
+						</ValidationProvider>
+						<ValidationProvider
+							v-slot="{ errors }"
+							name="パスワード"
+							rules="required|min:6|max:32"
+						>
+							<v-text-field
+								v-model="password"
+								label="パスワード"
+								:append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+								:type="show1 ? 'text' : 'password'"
+								@click:append="show1 = !show1"
+								:error-messages="errors"
+								required
+							/>
+						</ValidationProvider>
+						<v-card-actions>
+							<v-btn :disabled="invalid" 	@click="handleLogin">ログイン</v-btn>
+						</v-card-actions>
+					</v-form>
+				</ValidationObserver>
 			</v-card-text>
-			<v-card-actions>
-				<v-btn @click="handleLogin">ログイン</v-btn>
-			</v-card-actions>
 		</v-card>
 	</v-app>
 </template>
@@ -36,6 +62,9 @@ export default ({
 		...mapGetters(['user'])
 	},
 	methods: {
+		async submit () {
+      this.$refs.observer.validate()
+    },
 		async handleLogin(){
 			const formData = new FormData();
 			formData.append('email', this.email)
