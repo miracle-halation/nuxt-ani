4360242bd64519d3445cca25d0a958b72540d32b
miracle-halation <miraclehalation.9710@gmail.com>
2021-09-12 10:12:50 +0900

    signupページにバリデーションを適応

--- pages/signup/index.vue
+++ pages/signup/index.vue
@@ -5,32 +5,80 @@
 				<h1 class="display-1">新規登録</h1>
 			</v-card-title>
 			<v-card-text>
-				<v-form>
-					<v-text-field v-model="nickname" label="ニックネーム" />
-					<v-text-field v-model="email" label="メールアドレス" />
-					<v-text-field v-model="password" label="パスワード"
-						:append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
-            :type="show1 ? 'text' : 'password'"
-            @click:append="show1 = !show1"
-					 />
-					<v-text-field v-model="password_confirm" label="パスワード確認"
-						:append-icon="show2 ? 'mdi-eye' : 'mdi-eye-off'"
-            :type="show2 ? 'text' : 'password'"
-            @click:append="show2 = !show2"
-					 />
-					<v-file-input @change="setImage" label="アイコン" />
-				</v-form>
+				<ValidationObserver
+					ref="observer"
+					v-slot="{ invalid }"
+				>
+					<v-form @submit.prevent="submit">
+						<ValidationProvider
+							v-slot="{ errors }"
+							name="ニックネーム"
+							rules="required|max:10"
+						>
+							<v-text-field
+								v-model="nickname"
+								label="ニックネーム"
+								:counter="10"
+								:error-messages="errors"
+								required
+							/>
+						</ValidationProvider>
+						<ValidationProvider
+							v-slot="{ errors }"
+							name="メールアドレス"
+							rules="required|email"
+						>
+							<v-text-field
+								v-model="email"
+								label="メールアドレス"
+								:error-messages="errors"
+								required
+							/>
+						</ValidationProvider>
+						<ValidationProvider
+							v-slot="{ errors }"
+							name="パスワード"
+							rules="required|min:6|max:32"
+							vid="password"
+						>
+							<v-text-field
+								v-model="password"
+								label="パスワード"
+								:append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+								:type="show1 ? 'text' : 'password'"
+								@click:append="show1 = !show1"
+								:error-messages="errors"
+								required
+							/>
+						</ValidationProvider>
+						<ValidationProvider
+							v-slot="{ errors }"
+							name="パスワード"
+							rules="required|confirmed:password"
+						>
+							<v-text-field
+								v-model="password_confirm"
+								label="パスワード確認"
+								:append-icon="show2 ? 'mdi-eye' : 'mdi-eye-off'"
+								:type="show2 ? 'text' : 'password'"
+								@click:append="show2 = !show2"
+								:error-messages="errors"
+								required
+							/>
+						</ValidationProvider>
+						<v-file-input @change="setImage" label="アイコン" />
+						<v-card-actions>
+							<v-btn :disabled="invalid" @click="handleSignUp">新規登録</v-btn>
+						</v-card-actions>
+					</v-form>
+				</ValidationObserver>
 			</v-card-text>
-			<v-card-actions>
-				<v-btn @click="handleSignUp">新規登録</v-btn>
-			</v-card-actions>
 		</v-card>
 	</v-app>
 </template>
 
 <script>
 import {mapGetters, mapActions} from 'vuex'
-
 
 export default {
 	asyncData(){
@@ -48,6 +96,9 @@ export default {
 		...mapGetters(['user'])
 	},
 	methods: {
+		async submit () {
+      this.$refs.observer.validate()
+    },
 		async setImage(e){
 			this.icon = e
 		},
