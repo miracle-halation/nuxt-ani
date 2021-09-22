f1075bc7b184f4700b19761ba2315c1bec77a6d7
miracle-halation <miraclehalation.9710@gmail.com>
2021-09-22 16:12:50 +0900

    ルーム一覧ページのざっくりレイアウト作成

---
+++ pages/rooms/index.vue
@@ -0,0 +1,129 @@
+<template>
+  <div>
+    <v-container>
+      <!-- 自分の所属するルームを表示or傾向によるおすすめ表示 -->
+      <v-row><h1>おすすめグループ</h1></v-row>
+        <v-slide-group
+          v-model="model"
+          class="pa-4"
+          active-class="success"
+          show-arrows
+        >
+          <v-slide-item
+            v-for="n in 12"
+            :key="n"
+          >
+            <v-row no-gutters class="test">
+              <v-card
+                class="mx-auto"
+                max-width="340"
+              >
+                <v-img
+                  src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+                  height="200px"
+                ></v-img>
+                <v-card-title>
+                  Top western road trips
+                </v-card-title>
+                <v-card-subtitle>
+                  1,000 miles of wonder
+                </v-card-subtitle>
+              </v-card>
+            </v-row>
+          </v-slide-item>
+        </v-slide-group>
+
+      <!-- ここに最新のルームを表示 -->
+            <v-row><h1>最新ルーム</h1></v-row>
+        <v-slide-group
+          v-model="model"
+          class="pa-4"
+          active-class="success"
+          show-arrows
+        >
+          <v-slide-item
+            v-for="n in 12"
+            :key="n"
+          >
+            <v-row no-gutters class="test">
+              <v-card
+                class="mx-auto"
+                max-width="340"
+              >
+                <v-img
+                  src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+                  height="200px"
+                ></v-img>
+                <v-card-title>
+                  Top western road trips
+                </v-card-title>
+                <v-card-subtitle>
+                  1,000 miles of wonder
+                </v-card-subtitle>
+              </v-card>
+            </v-row>
+          </v-slide-item>
+        </v-slide-group>
+
+        <!-- ここにワンクリック検索用の画像をはる -->
+      <v-row><h1>ジャンル</h1></v-row>
+      <v-row no-gutters >
+        <v-card
+          v-for="n in 9"
+          :key="n"
+          class="mx-auto"
+          max-width="300"
+        >
+          <v-img
+            src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+            height="200px"
+          ></v-img>
+          <v-card-title>
+            Top western road trips
+          </v-card-title>
+          <v-card-subtitle>
+            1,000 miles of wonder
+          </v-card-subtitle>
+        </v-card>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+	asyncData(){
+		return{
+      model: null,
+		}
+	},
+}
+</script>
+
+<style scoped>
+
+h1{
+  margin: 2rem auto;
+}
+
+.mx-auto{
+  margin: 2rem;
+  padding: 2rem;
+}
+
+.pa-4{
+  margin-bottom: 7rem;
+}
+
+@media screen and (max-width: 959px) {
+	.test{
+    margin-left: 1.5rem;
+    margin-right: 3.5rem;
+    width: 100%;
+  }
+}
+@media screen and (max-width: 480px) {
+
+}
+
+</style>
