b275fb260957b984269bcf8aee43d14a1f001db3
miracle-halation <miraclehalation.9710@gmail.com>
2021-09-30 11:18:45 +0900

    詳細ページ（仮）作成

---
+++ pages/rooms/_id.vue
@@ -0,0 +1,34 @@
+<template>
+	<div>
+    ID is {{ $route.params.id }}
+		{{room}}
+  </div>
+</template>
+
+<script>
+export default {
+	asyncData(){
+		return{
+			room: null
+		}
+	},
+	mounted(){
+		this.fetchRoom()
+	},
+	methods:{
+		async fetchRoom(){
+			await this.$axios.get('/v1/rooms', this.$route.params.id)
+			.then((response) => {
+				this.room = response.data.data
+			})
+			.catch((error) => {
+				console.log(error)
+			})
+		}
+	}
+}
+</script>
+
+<style scoped>
+
+</style>
