68a5609cfa3c9aa80bbbca60c3504f7f38edf369
miracle-halation <miraclehalation.9710@gmail.com>
2021-10-21 21:33:01 +0900

    profileに自分の情報を表示
    ファイル名も合わせて修正

--- pages/user/_id/profile.vue
+++ pages/user/_id/profile.vue
@@ -1,85 +1,143 @@
 <template>
-	<v-app>
-		<v-card>
-			<v-card-title>
-				<h1 class="display-1">プロフィール変更</h1>
-			</v-card-title>
-			<v-card-text>
-				<ValidationObserver
-					ref="observer"
-					v-slot="{ invalid }"
+	<!-- 公開用プロフィールをここに表示する。本人なら、編集ボタンなどを表示する -->
+	<v-row justify="center">
+    <v-col
+      cols="12"
+      sm="8"
+			class="user_info"
+    >
+			<v-img
+				src="https://picsum.photos/700?image=996"
+				height="200px"
+			></v-img>
+			<v-card>
+				<v-tabs
+					v-model="tab"
+					background-color="deep-purple accent-4"
+					centered
+					dark
+					icons-and-text
 				>
-					<v-form @submit.prevent="submit">
-						<ValidationProvider
-							v-slot="{ errors }"
-							name="居住地域"
-							rules="required|max:50"
-						>
-							<v-text-field
-								v-model="address"
-								label="居住地域"
-								placeholder="例)日本 東京"
-								:counter="50"
-								:error-messages="errors"
-								required
-							/>
-						</ValidationProvider>
-						<ValidationProvider
-							v-slot="{ errors }"
-							name="お気に入り作品"
-							rules="max:50"
-						>
-								<v-text-field
-									v-model="tag_name"
-									label="お気に入り作品"
-									:counter="50"
-									:error-messages="errors"
-								/>
-								<v-btn @click="AddTags">お気に入りを追加する</v-btn>
-								<div class="tags-field">
-									<div class="text-center" v-for="(tag, index) in tags" :key="index">
-										<v-chip
-											class="ma-2"
-											close
-											@click:close="RemoveTag(index)"
-										>
-											{{tag}}
-										</v-chip>
-									</div>
-								</div>
-						</ValidationProvider>
-						<ValidationProvider
-							v-slot="{ errors }"
-							name="自己紹介"
-							rules="required|max:300"
-						>
-							<v-textarea
-								v-model="myinfo"
-								label="自己紹介"
-								:counter="300"
-								:error-messages="errors"
-								required
-							/>
-						</ValidationProvider>
-						<v-card-actions>
-							<v-btn :disabled="invalid" @click="UpdateProfile">プロフィールを更新する</v-btn>
-						</v-card-actions>
-					</v-form>
-				</ValidationObserver>
-			</v-card-text>
-		</v-card>
-	</v-app>
+					<v-tabs-slider></v-tabs-slider>
+
+					<v-tab href="#tab-1">
+						Profile
+						<v-icon>mdi-phone</v-icon>
+					</v-tab>
+
+					<v-tab href="#tab-2">
+						Favorites Rooms
+						<v-icon>mdi-heart</v-icon>
+					</v-tab>
+
+					<v-tab href="#tab-3">
+						Friends
+						<v-icon>mdi-account-box</v-icon>
+					</v-tab>
+				</v-tabs>
+
+				<v-tabs-items v-model="tab">
+					<v-tab-item value="tab-1">
+						<v-card>
+							<v-card-title class="cyan darken-1">
+								<span class="text-h5 white--text">{{user.user.nickname}}</span>
+
+								<v-spacer></v-spacer>
+
+								<nuxt-link :to="`/user/${user.user.id}/edit`">
+									<v-btn
+										dark
+										icon
+									>
+										<v-icon>mdi-pencil</v-icon>
+									</v-btn>
+								</nuxt-link>
+
+								<v-btn
+									dark
+									icon
+								>
+									<v-icon>mdi-dots-vertical</v-icon>
+								</v-btn>
+							</v-card-title>
+
+							<v-list>
+								<v-list-item>
+									<v-list-item-action>
+										<v-icon>mdi-email</v-icon>
+									</v-list-item-action>
+
+									<v-list-item-content>
+										<v-list-item-title>{{user.user.email}}</v-list-item-title>
+									</v-list-item-content>
+								</v-list-item>
+
+								<v-divider inset></v-divider>
+
+								<v-list-item>
+									<v-list-item-action>
+										<v-icon>mdi-map-marker</v-icon>
+									</v-list-item-action>
+
+									<v-list-item-content>
+										<v-list-item-title>{{user.user.address}}</v-list-item-title>
+									</v-list-item-content>
+								</v-list-item>
+
+								<v-divider inset></v-divider>
+
+								<v-list-item>
+									<v-list-item-action>
+										<v-icon>mdi-message-text</v-icon>
+									</v-list-item-action>
+
+									<v-list-item-content>
+										<v-list-item-title>{{user.user.myinfo}}</v-list-item-title>
+									</v-list-item-content>
+								</v-list-item>
+							</v-list>
+
+						</v-card>
+					</v-tab-item>
+					<v-tab-item value="tab-2">
+						<v-list v-for="(tag, index) in tags" :key="index">
+							<v-list-item>
+
+								<v-list-item-content>
+									<v-list-item-title>{{tag}}</v-list-item-title>
+								</v-list-item-content>
+							</v-list-item>
+						</v-list>
+					</v-tab-item>
+					<v-tab-item value="tab-3">
+						<v-list>
+							<v-list-item>
+								<v-list-item-action>
+									<v-icon>mdi-phone</v-icon>
+								</v-list-item-action>
+
+								<v-list-item-content>
+									<v-list-item-title>(650) 555-1234</v-list-item-title>
+								</v-list-item-content>
+								<v-list-item-action>
+									<v-icon>mdi-message-text</v-icon>
+								</v-list-item-action>
+							</v-list-item>
+						</v-list>
+					</v-tab-item>
+				</v-tabs-items>
+			</v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
 import {mapGetters, mapActions} from 'vuex'
 export default {
-	asyncData(){
+	asyncData () {
 		return {
-			address: "",
-			tag_name: "",
 			tags: [],
-			myinfo: ""
+			tab: null
 		}
 	},
 	computed:{
@@ -96,52 +154,19 @@ export default {
 					for(let i=0;i<tags.length;i++){
 						this.tags.push(tags[i]['name'])
 					}
-					this.address = this.user.user.address
-					this.myinfo = this.user.user.myinfo
 				},
 				(error) => {
 					console.log(error)
 				})
 		},
-		async submit () {
-      this.$refs.observer.validate()
-    },
-		async AddTags(){
-			if(this.tag_name !== ""){
-				this.tags.push(this.tag_name)
-				this.tag_name = ""
-			}
-		},
-		async RemoveTag(index){
-			this.tags.splice(index, 1)
-		},
-		async UpdateProfile(){
-			const formData = new FormData()
-			formData.append('address', this.address)
-			formData.append('myinfo', this.myinfo)
-			formData.append('tags', this.tags)
-			formData.append('id', this.user.user.id)
-			await this.$axios.patch(`/auth/`, formData)
-			.then((response) => {
-				this.$router.push('/rooms')
-			},
-			(error) => {
-				this.showMessage({
-					message: "作成に失敗しました",
-					type: "red",
-					status: true
-				})
-			})
-		},
-		...mapActions('flashMessage', ['showMessage'])
 	}
 }
 </script>
 
 <style scoped>
-	.tags-field{
-		display: flex;
-		flex-wrap: wrap;
+
+	.user_info{
+		margin-top: 3rem;
 	}
 
 </style>
