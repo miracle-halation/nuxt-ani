98bcb5e2ec76c56915b153fac8241bc7ca9a848b
miracle-halation <miraclehalation.9710@gmail.com>
2021-11-23 11:24:06 +0900

    ルーム詳細をコンポーネント化

--- pages/rooms/_id/index.vue
+++ pages/rooms/_id/index.vue
@@ -1,73 +1,6 @@
 <template>
 	<v-row no-gutters justify="space-between">
-		<v-card
-			class="mx-auto"
-			max-width="300"
-			tile
-		>
-			<v-list dense >
-				<v-list-item-title class="list-title">{{room.name}}</v-list-item-title>
-				<v-list-item-group>
-					<v-list-item>
-						<v-list-item-content>
-							<v-list-item-action-text>{{room.description}}</v-list-item-action-text>
-						</v-list-item-content>
-					</v-list-item>
-					<v-list-item>
-						<v-list-item-content>
-							<v-list-item-action-text>{{room.leader}}</v-list-item-action-text>
-						</v-list-item-content>
-					</v-list-item>
-					<v-list-item>
-						<v-list-item-content>
-							<v-list-item-action-text v-if="`${room.private}`">パブリック</v-list-item-action-text>
-							<v-list-item-action-text v-else>プライベート</v-list-item-action-text>
-						</v-list-item-content>
-					</v-list-item>
-					<v-list-item
-						v-if="!this.users.some((ele) => ele.id === this.user.user.id)"
-					>
-						<v-btn
-							depressed
-							color="primary"
-							@click="joinUser"
-						>
-							加入申請
-						</v-btn>
-					</v-list-item>
-					<v-list-item v-if="`${user.user.nickname}` === `${room.leader}`">
-						<nuxt-link :to="room.id + '/edit'">
-						<v-btn
-							depressed
-							color="success"
-						>
-							編集
-						</v-btn>
-						</nuxt-link>
-					</v-list-item>
-					<v-list-item v-if="`${user.user.nickname}` === `${room.leader}`">
-						<v-btn
-							depressed
-							color="error"
-							@click="deleteRoom"
-						>
-							ルーム削除
-						</v-btn>
-					</v-list-item>
-					<v-list-item
-						v-if="this.users.some((ele) => ele.id === this.user.user.id)"
-					>
-						<v-btn
-							depressed
-							color="error"
-							@click="departUser"
-						>
-							脱退
-						</v-btn>
-					</v-list-item>
-				</v-list-item-group>
-			</v-list>
-		</v-card>
+		<DetailMenu :room="this.room" :users="this.users" :current_user="this.user.user"></DetailMenu>
 
 		<v-col
 			class="chat-space"
@@ -215,9 +148,13 @@
 
 <script>
 import {mapGetters, mapActions} from 'vuex'
+import DetailMenu from '@/components/room/DetailMenu.vue'
 
 export default {
 	middleware: 'authenticated',
+	components:{
+		DetailMenu
+	},
 	asyncData(){
 		return{
 			room: [],
