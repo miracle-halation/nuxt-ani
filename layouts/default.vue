a0cb92b80725486be39211709110a59a48c97bc9
miracle-halation <miraclehalation.9710@gmail.com>
2022-09-23 10:10:53 +0900

    サイドバーに自分が所属しているルーム一覧を表示する

--- layouts/default.vue
+++ layouts/default.vue
@@ -33,9 +33,9 @@
           router
           exact
         >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
+          <v-list-item-avatar>
+            <v-img :src="item.icon"></v-img>
+          </v-list-item-avatar>
           <v-list-item-content>
             <v-list-item-title v-text="item.title" />
           </v-list-item-content>
@@ -48,14 +48,24 @@
       app
     >
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <template v-if="isLoggedIn">
-        <nuxt-link :to="`/user/${user_id}/profile`">
-          <img  :src="icon" class="user-image">
-        </nuxt-link>
-        <v-btn color="green" @click="handleLogout">ログアウト</v-btn>
-      </template>
+        <template v-if="isLoggedIn">
+          <nuxt-link :to="`/rooms`" tag="div">
+            <v-toolbar-title v-text="title" />
+          </nuxt-link>
+          <nuxt-link :to="`/rooms/new`" tag="div">
+            <v-btn color="blue" class="new_room">新規ルーム作成</v-btn>
+          </nuxt-link>
+          <v-spacer />
+          <nuxt-link :to="`/user/${user_id}/profile`">
+            <img :src="icon" class="user-image">
+          </nuxt-link>
+          <v-btn color="green" @click="handleLogout">ログアウト</v-btn>
+        </template>
+        <template v-else>
+          <nuxt-link :to="`/`">
+            <v-toolbar-title v-text="title" />
+          </nuxt-link>
+        </template>
     </v-app-bar>
     <v-main>
       <v-container class="main-container" fluid>
@@ -102,18 +112,7 @@ export default {
           to: '/signup'
         }
       ],
-      login_items: [
-        {
-          icon: 'mdi-apps',
-          title: 'トップへ',
-          to: '/rooms'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: '新規ルーム作成',
-          to: '/rooms/new'
-        }
-      ],
+      login_items: [],
       miniVariant: false,
       mobile_breakpoint: 1904,
       title: 'オリジナルアプリ'
@@ -122,7 +121,27 @@ export default {
   computed:{
     ...mapGetters('user', ['user_id','isLoggedIn', 'icon'])
 	},
+  mounted(){
+    this.fetchRooms()
+  },
   methods:{
+    fetchRooms(){
+      this.$axios.get('v1/rooms')
+      .then((response) => {
+        const rooms = response.data.data[0]
+        for(let i=0;i<rooms.length;i++){
+          this.login_items.push({
+            icon: rooms[i]['image_path'],
+            title: rooms[i]['name'],
+            to: '/rooms/'+rooms[i]['id']
+          })
+        }
+        console.log(this.login_items)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+    },
     async handleLogout(){
       await this.logout()
       this.$router.go('/login')
@@ -141,5 +160,8 @@ export default {
     width:  50px;
     height: 50px;
     margin-right: 2rem;
+  }
+  .new_room{
+    margin-left: 1rem;
   }
 </style>
